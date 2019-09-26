9ed050228ef30916fc9fc47db22c98437957b0bb
shorticins <shanendibbern@gmail.com>
2019-09-26 02:14:11 +0000

    initial commit

---
+++ labwork/lab5/index.php
@@ -0,0 +1,11 @@
+<?php //php -S localhost:3000 ?>
+<?php include('includes/header.php');
+    //include pastes header.php on top of this file
+?>
+
+        <main>
+            <h1>This is the homepage</h1>
+
+        </main>
+
+<?php include('includes/footer.php');
