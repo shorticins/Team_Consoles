7b7a816f0f56a61b6341df6ddb9d771053f1b6d5
shorticins <shanendibbern@gmail.com>
2019-09-27 21:44:24 +0000

    Initial footer + styles

--- homework/hw5/footer.php
+++ homework/hw5/footer.php
@@ -1,6 +1,14 @@
          <!--START OF FOOTER-->
         <footer>
-            <p>Footer</p>
+        <nav>
+         <nav>
+            <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Log in</a></li>
+            </ul>
+            </nav>
+            <p>&copy; Team Consoles COSW-30</p>
         </footer>
     </body>
 </html>
