b395a71b38f95acc49a30cb308c48447f6b76d45
shorticins <shanendibbern@gmail.com>
2019-09-27 15:51:31 +0000

    Added some fonts, basic css and logo
    Signed-off-by: shorticins <shanendibbern@gmail.com>

--- homework/hw5/header.php
+++ homework/hw5/header.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <title>Homepage</title>
         <link href="styles.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bangers|Gloria+Hallelujah&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
+        <img id="logo" src="images/logo.png" alt="silly cartoon face">
             <p>Header</p>
         </header>
         <!--END OF HEADER-->
