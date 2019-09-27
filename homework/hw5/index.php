96578dbb778daa757dcfe772fb60df3e2e89093a
developergrace <gpark2351@gmail.com>
2019-09-27 01:01:55 +0000

    draft index.php and images
    Signed-off-by: developergrace <gpark2351@gmail.com>

---
+++ homework/hw5/index.php
@@ -0,0 +1,35 @@
+<?php
+    //Grace is working on this
+    //php -S localhost:3000
+?>
+<?php
+    include('includes/header.php');
+?>
+        <main>
+            <h1>Creep It Real</h1>
+
+            <div class="header_img">
+                <img src="images/skull.jpg" alt="Skull and Pumpkins" width="900">
+            </div>
+            <div class="categories" style="display:inline-block">
+                <h2>Couples & Groups</h2>
+                <img src="images/costume.jpg" alt="Costumed Couple" width="300">
+            </div>
+            <div class="categories" style="display:inline-block">
+                <h2>Pet Costumes</h2>
+                <img src="images/dog.jpg" alt="Dog Costume" width="300">
+            </div>
+            <div class="categories" style="display:inline-block">
+                <h2>TV & Movie</h2>
+                <img src="images/got.jpg" alt="Game of Thrones Costumes" width="300">
+            </div>
+
+            <p>Make them scream with envy over our costumes. Creep It Real is the number one source of high-quality
+            Halloween costumes. From kings to pirates, movie characters, and more, we have everything to make you stand out from the
+            crowd. Register today to browse our selection of exclusive styles!</p>
+            <p>If you've got it, haunt it.</p>
+            <a href="">Register</a>
+
+        </main>
+
+<?php include('includes/footer.php');
