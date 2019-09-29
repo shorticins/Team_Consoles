d070fd514c10147b4d3946c18c7b719ada3df49d
shorticins <shanendibbern@gmail.com>
2019-09-29 07:56:52 +0000

    restored login.php

--- homework/hw5/login.php
+++ homework/hw5/login.php
@@ -1,8 +1,7 @@
-
 <?php
-/*Please add login markup for registration form and php code for validation...
+/*Please add login markup for login form and php code for validation...
 remember to include header and footer - thx--Shanen
-    Add login markup for registration form and php code for validation...
+    Add login markup for login form and php code for validation...
     including header and footer - Oanh
 */
 ?>
@@ -12,76 +11,51 @@ remember to include header and footer - thx--Shanen
 
 <?php
     // declare variables
-    $f_name = "";
-    $l_name = "";
     $email = "";
     $password = "";
     $conf_password ="";
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         // store post data into variables
-        $f_name = $_POST['f_name'];
-        $l_name = $_POST['l_name'];
         $email = $_POST['email'];
         $password = $_POST['password'];
         $conf_password = $_POST['conf_password'];
-
         // validate email field
-        if(empty($f_name)) {
-            $error_f = "This field is required. Please enter your first name.";
+        if(empty($email)) {
+            $error_e = "This field is required. Please enter your email";
         }
-        elseif(empty($l_name)) {
-            $error_l = "This field is required. Please enter your last name.";
-        }
-        elseif(empty($email)) {
-            $error_e = "This field is required. Please enter your email.";
-        }
-        elseif(empty($password)) {
-            $error_p = "This field is required. Please enter your password.";
+         elseif(empty($password)) {
+            $error_p = "This field is required. Please enter your password";
         }
         elseif(empty($conf_password)) {
-            $error_cp = "This field is required. Please re-enter your password.";
+            $error_cp ="This field is required. Please re-enter your password";
         }
         else{
+
         }
     }
 ?>
-
 <main>
-    <h1>Registration Form</h1>
-    <form action="register.php" method="POST">
-    <label for="f_name">First Name:</label>
-    <input type="text" id="f_name" name="f_name" value="<?php echo $f_name; ?>"></br>
+    <h1>Login Page</h1>
+    <form action="login.php" method="POST">
 
-    <?php // if first name is not entered, display error message
-        if(isset($error_f)) {
-            echo "<p>$error_f</p>";
-        }
-    ?>
-    <label for="l_name">Last Name:</label>
-    <input type="text" id="l_name" name="l_name" value="<?php echo $l_name; ?>"></br>
-    <?php // if last name is not entered, display error message
-        if(isset($error_l)) {
-            echo "<p>$error_l</p>";
-        }
-    ?>
-    <label for="email">Email</label>
+    <label for="email">Email:</label>
     <input type="email" id="email" name="email" placeholder="test@test.com" value="<?php echo $email; ?>"></br>
     <?php // if email is not entered, display error message
         if(isset($error_e)) {
             echo "<p>$error_e</p>";
         }
     ?>
-    <label for="password">Password</label>
+    <label for="password">Password:</label>
     <input type="password" id="password" name="password"></br>
     <?php // if password is not entered, display error message
         if(isset($error_p)) {
-            echo "<p>$erro_pr</p>";
+            echo "<p>$error_p</p>";
         }
     ?>
-    <label for="conf_password">Confirm Your Password</label>
+    <label for="password">Confirm Your Password:</label>
     <input type="password" id="conf_password" name="conf_password"></br>
-    <?php // if confirmed is not entered, display error message
-        if(isset($error_cp)) {
+    <?php // if password is not entered, display error message
+        if(isset($error_p)) {
             echo "<p>$error_cp</p>";
         }
         elseif($conf_password != $password) {
@@ -92,16 +66,13 @@ remember to include header and footer - thx--Shanen
         }
     ?>
 
-    <input type="submit" name="register_button" value="Register to Creep It Real!">
+    <input type="submit" name="login_button" value="Login to Creep It Real!">
 
     <?php // display message when submit button is clicked and all fields are correctly entered
-    if ((isset($_POST['register_button']))  && (!isset($error_f)) && (!isset($error_l))&& (!isset($error_e)) && (!isset($error_p)) && (!isset($error_cp)) && ($conf_password == $password)) {
-    echo "<p>Congratulations!!! You are registered to Creep It Real!</p>";
-    echo "<p>Look for our special announcements in your inbox.</p>";
+    if ((isset($_POST['login_button'])) && (!isset($error_e)) && (!isset($error_p)) && (!isset($error_cp)) && ($conf_password == $password)) {
+    echo "<p>You're successfully logged in.</p>";
     }
 ?>
-
-
     </form>
 </main>
 
