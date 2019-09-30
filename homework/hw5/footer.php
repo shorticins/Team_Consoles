c2a80342084c8bcd8c47f1f1cd63c12ec1c50724
shorticins <shanendibbern@gmail.com>
2019-09-30 19:28:14 +0000

    css edit for footer nav and main -SD

--- homework/hw5/footer.php
+++ homework/hw5/footer.php
@@ -2,8 +2,8 @@
 
         <footer>
         <div class="footer">
-         <nav>
-              <ul class="footer_links"> 
+         <nav id="footer_nav>
+              <ul class="footer_links">
                 <li class="footer_link"><a href="index.php">Home</a></li>
                 <li class="footer_link"><a href="register.php">Register</a></li>
                 <li class="footer_link"><a href="login.php">Log in</a></li>
